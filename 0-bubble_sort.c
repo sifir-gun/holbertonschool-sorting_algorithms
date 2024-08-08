8c2d6824072f431d4eb0ce1edfaa80519271c664
zyclamenace <gemicieneshakki@gmail.com>
2024-08-08 14:25:50 +0200

    fix .gitignore and finished task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,17 @@
 #include "sort.h"
 /**
+ * swap - Échange deux éléments d'un tableau
+ * @a: premier élément
+ * @b: deuxième élément
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * bubble_sort - sort with the bubble technique
  * @array: tableau
  * @size: taille
@@ -8,5 +20,21 @@
 */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
 }
