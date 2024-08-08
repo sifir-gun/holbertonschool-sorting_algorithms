c74c2be0a8e55768342f6042b3f267937697b3c8
sifir-gun <guney.tasdelen74@gmail.com>
2024-08-08 15:53:27 +0200

    add task 3 + Fix quick_sort to correctly print array after each swap

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,6 +13,41 @@ void swap(int *a, int *b)
 }
 
 /**
+ * partition - Partitions the array using Lomuto partition scheme.
+ * @array: The array to partition.
+ * @low: The starting index of the sub-array.
+ * @high: The ending index of the sub-array.
+ * @size: The size of the array.
+ *
+ * Return: The index of the pivot.
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
+	if (array[high] < array[i + 1])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
  * quick_sort_recursive - Recursively sorts an array using Quick sort.
  * @array: The array to sort.
  * @low: The starting index of the sub-array.
@@ -23,29 +58,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		int pivot = array[high];
-		int i = low - 1;
-		int j;
-
-		for (j = low; j < high; j++)
-		{
-			if (array[j] <= pivot)
-			{
-				i++;
-				if (i != j)
-				{
-					swap(&array[i], &array[j]);
-					print_array(array, size);
-				}
-			}
-		}
-		if (array[high] < array[i + 1])
-		{
-			swap(&array[i + 1], &array[high]);
-			print_array(array, size);
-		}
-
-		int pivot_index = i + 1;
+		int pivot_index = partition(array, low, high, size);
 
 		quick_sort_recursive(array, low, pivot_index - 1, size);
 		quick_sort_recursive(array, pivot_index + 1, high, size);
@@ -53,11 +66,9 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 }
 
 /**
- * quick_sort - Sorts an array of integers in ascending order
- * using the Quick sort algorithm
- *
- * @array: Pointer to the array to sort
- * @size: Size of the array
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort.
+ * @array: The array to sort.
+ * @size: The size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
