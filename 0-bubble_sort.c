6864038f75234fbf41261a26e16239877dc513a8
zyclamenace <gemicieneshakki@gmail.com>
2024-08-08 10:14:49 +0200

    adding the beginning of task 0 and 1

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * bubble_sort - sort with the bubble technique
+ * @array: tableau
+ * @size: taille
+ *
+ * Return: array sort by the bubble technique
+*/
+void bubble_sort(int *array, size_t size)
+{
+
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort with the bubble technique
+ * @list: tableau
+ *
+ * Return: array sort by the bubble technique
+*/
+void insertion_sort_list(listint_t **list)
+{
+
+}
