f3599fed53d643604392e5573028e7aecc6fa040
zyclamenace <gemicieneshakki@gmail.com>
2024-08-08 14:55:29 +0200

    fix *-O files, added prototype for task 3 and started task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/**
+ * quick_sort - sorts array of integers ascending order using Quick sort
+ * @array: le tableau
+ * @size: taille
+ *
+ * Return:
+*/
+void quick_sort(int *array, size_t size)
+{
+
+}
